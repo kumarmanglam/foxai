4ff1cee8e0be1fd3f8abd695d35d1187476c7e46
kumarmanglam <kumarthe799@gmail.com>
2024-09-22 18:28:14 +0530

    feat: updated auth routes

--- aifox_backend/controller/authController.js
+++ aifox_backend/controller/authController.js
@@ -40,8 +40,7 @@ const loginController = async (req, res) => {
     // Create JWT token
     const token = await JWT.sign(
       { email: user.email_id },
-      process.env.JWT_SECRET_KEY,
-
+      "foxai",
       { expiresIn: "7d" }
     );
 
@@ -72,61 +71,60 @@ const loginController = async (req, res) => {
 
 //add user
 
-const addUserController=async(req,res)=>{
-
-
-  try{
-    const {name,email_id,password,department,role,phone_number}= req.body;
+const addUserController = async (req, res) => {
+  try {
+    const { name, email_id, password, department, role, phone_number } = req.body;
     const hashedPassword = await hashPassword(password);
 
-  const newPerson = new Person({
-    name,
-    password:hashedPassword,
-    email_id,
-    department,
-    role,phone_number
-  });
-  console.log(newPerson)
-  const response = await newPerson.save();
-  console.log("data saved");
-  res.status(200).json({response});
-} catch (err) {
-  res.status(500).json({ error: "internal server" });
-}
+    const newPerson = new Person({
+      name,
+      password: hashedPassword,
+      email_id,
+      department,
+      role,
+      phone_number
+    });
+    console.log(newPerson)
+    const response = await newPerson.save();
+    console.log("data saved");
+    res.status(200).json({ response });
+  } catch (err) {
+    res.status(500).json({ error: "internal server" });
+  }
 }
 
 //update user
 
-  const updateUserController = async (req, res) => {
-    try {
-      const email_id= req.params.email_id;
-      console.log(email_id)
-      const updates = req.body;
-      const updatedUser = await Person.findOneAndUpdate(
-
-        { email_id: email_id},
-        updates
-      );
-
-
-  console.log(updatedUser)
-      if (!updatedUser) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.status(200).json({ success: true, data: updatedUser });
-    } catch (error) {
-      console.error('Error updating user:', error);
-      res.status(500).json({ success: false, message: 'Internal server error', error: error.message });
-    }
-  };
-
- //delete
- const deleteUserController=async(req, res)=>{
+const updateUserController = async (req, res) => {
   try {
-    const email_id= req.params.email_id;
-    console.log({email:email_id})
+    const email_id = req.params.email_id;
+    console.log(email_id)
+    const updates = req.body;
+    const updatedUser = await Person.findOneAndUpdate(
 
-    const deletedUser = await Person.findOneAndDelete({email_id: email_id});
+      { email_id: email_id },
+      updates
+    );
+
+
+    console.log(updatedUser)
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ success: true, data: updatedUser });
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).json({ success: false, message: 'Internal server error', error: error.message });
+  }
+};
+
+//delete
+const deleteUserController = async (req, res) => {
+  try {
+    const email_id = req.params.email_id;
+    console.log({ email: email_id })
+
+    const deletedUser = await Person.findOneAndDelete({ email_id: email_id });
 
 
     if (!deletedUser) {
@@ -137,17 +135,17 @@ const addUserController=async(req,res)=>{
   } catch (err) {
     res.status(500).json({ error: "Internal server error" });
   }
- }
+}
 
 
 
- //get all user
+//get all user
 
- const getAllUserController=async(req,res)=>{
+const getAllUserController = async (req, res) => {
   try {
     const users = await Person.find();
 
-console.log({users})
+    console.log({ users })
     if (users.length === 0) {
       return res.status(404).json({ message: "No users found" });
     }
@@ -156,17 +154,17 @@ console.log({users})
   } catch (err) {
     res.status(500).json({ error: "Internal server error" });
   }
- }
+}
 
 
- //get user info
+//get user info
 
- const getUserController=async(req,res)=>{
+const getUserController = async (req, res) => {
   try {
     const email_id = req.params.email_id;
-    console.log({email_id})
-    const user = await Person.findOne({email_id});
-    console.log({user})
+    console.log({ email_id })
+    const user = await Person.findOne({ email_id });
+    console.log({ user })
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -176,7 +174,7 @@ console.log({users})
   } catch (err) {
     res.status(500).json({ error: "Internal server error" });
   }
- }
+}
 
 module.exports = {
   addUserController,
--- aifox_backend/routes/documentsRoutes.js
+++ aifox_backend/routes/documentsRoutes.js
@@ -6,7 +6,6 @@ const router = Router();
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
-
 router.delete('/deleteDocs', deleteFile);
 router.post('/uploadDocs', upload.single('pdf'), uploadDocs);
 router.get('/getDocsByDept', getDocsByDeptController);
